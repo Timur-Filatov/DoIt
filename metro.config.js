e779997b418bf32c48310cfb66cd972eed9e943d
Timur Filatov <timur.filatov@anuitex.net>
2024-03-15 16:15:33 +0200

    Changed settings to turn on Hermes

--- metro.config.js
+++ metro.config.js
@@ -6,6 +6,17 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+
+const config = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+    sourceMaps: true,
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
